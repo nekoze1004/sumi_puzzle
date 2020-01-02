e4fbdbee0aa73055865ef864f0b00ca7cf79099e
Masahiro Takasaki <52184699+carrymasa@users.noreply.github.com>
2020-01-03 02:27:17 +0900

    大型アップデート

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     //アニメーションスピード
-    var scrollSpeed = 1;
+    var scrollSpeed = 0.5;
     //画像サイズ
-    var imgWidth = 580;
-    var imgHeight = 500;
+    var imgWidth = 1000;
+    var imgHeight = 1000;
     //画像の初期位置
     var posX = 0;
     var posY = 0;
@@ -11,9 +11,9 @@ $(document).ready(function () {
     setInterval(function () {
         //画像のサイズまで移動したら0に戻る。
         if (posX >= imgWidth) posX = 0;
+        posX += scrollSpeed;
         if (posY >= imgHeight) posY = 0;
         //scrollSpeed分移動
-        posX += scrollSpeed;
         posY += scrollSpeed;
         $('body').css("background-position", posX + "px " + posY + "px");
 
