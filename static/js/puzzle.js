aaff5adcb4b5d353fab5fa6965f29b9a0ad691f4
Masahiro Takasaki <52184699+carrymasa@users.noreply.github.com>
2020-01-02 23:57:02 +0900

    ｊｓこみっと

--- static/js/puzzle.js
+++ static/js/puzzle.js
@@ -1,15 +1,31 @@
-$(function(){
-  $("#start").on("click", function(){
+
+$(function () {
+  var element = $("#face"); // 画像化したい要素をセレクタに指定
+var getCanvas;
+  $("#btn-Preview-Image").hide();
+  $("#start").on("click", function () {
     $(".parts").draggable();
     // $("#mekakusi").show();
     $(this).hide();
     $("#kansei").show();
   });
-  
-  $("#kansei").on("click", function(){
+
+  $("#kansei").on("click", function () {
     // $("#mekakusi").hide();
     $(".parts").draggable("disable");
     $("#kansei").hide();
+    $("#btn-Preview-Image").show();
     $("#post").show();
   });
+
+  //プレビュー
+$("#btn-Preview-Image").on('click', function () {
+  html2canvas(element, {
+    onrendered: function (canvas) {
+      $("#previewImage").append(canvas);
+      getCanvas = canvas;
+    }
+  });
 });
+});
+
