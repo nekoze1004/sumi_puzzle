a3c980eca4fb1d9efaf416d7ea4bc8ba549cfafc
Masahiro Takasaki <52184699+carrymasa@users.noreply.github.com>
2020-01-07 18:10:01 +0900

    Add files via upload

--- static/js/puzzle.js
+++ static/js/puzzle.js
@@ -1,40 +1,34 @@
-$(function () {
-
-  $("#start").on("click", function () {
-    $(".parts").draggable();
-    // $("#mekakusi").show();
-    $(this).hide();
-    $("#kansei").show();
-  });
-
-  $("#kansei").on("click", function () {
-    // $("#mekakusi").hide();
-    $(".parts").draggable("disable");
-    $("#kansei").hide();
-    $("#btn-Preview-Image").show();
-    $("#post").show();
-  });
-});
-$(document).ready(function () {
-
-
-  var element = $("#images"); // global variable
-  var getCanvas; // global variable
-
-  $("#btn-Preview-Image").on('click', function () {
-    html2canvas(element, {
-      onrendered: function (canvas) {
-        $("#previewImage").append(canvas);
-        getCanvas = canvas;
-      }
+$(function() {
+    $("#start").on("click", function() {
+        $(".parts").draggable();
+        // $("#mekakusi").show();
+        $(this).hide();
+        $("#kansei").show();
     });
-  });
-
-  $("#btn-Convert-Html2Image").on('click', function () {
-    var imgageData = getCanvas.toDataURL("image/png");
-    // Now browser starts downloading it instead of just showing it
-    var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-    $("#btn-Convert-Html2Image").attr("download", "hogehoge.png").attr("href", newData);
-  });
+    $("#kansei").on("click", function() {
+        // $("#mekakusi").hide();
+        $(".parts").draggable("disable");
+        $("#kansei").hide();
+        $("#Pre").show();
+        $("#post").show();
+    });
+    var element = $("#images"); // global variable
+    var getCanvas; // global variable
+    $("#Pre").on('click', function() {
+        html2canvas(element, {
+            onrendered: function(canvas) {
+                $("#previewImage").append(canvas);
+                getCanvas = canvas;
+            }
+        });
+        $(this).hide();
+        $('#download').show();
+    });
+    $("#download").on('click', function() {
+        var imgageData = getCanvas.toDataURL("image/png");
+        // Now browser starts downloading it instead of just showing it
+        var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+        $("#download").attr("download", "hogehoge.png").attr("href", newData);
+    });
 
 });
